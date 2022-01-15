c97c7086f99de2f49641c97a9f0d3775bfd04e94
tojocorm <63367603+tojocorm@users.noreply.github.com>
2022-01-15 16:05:43 -0500

    good stuff:

--- poker_chip_counter/poker_chip_counter/globals.cpp
+++ poker_chip_counter/poker_chip_counter/globals.cpp
@@ -1,191 +1,68 @@
 #include "globals.h"
 
-std::string get_type(piece piece){
-    switch(piece.type) {
-        case Pawn :
-            return "Pawn";
-        case Bishop :
-            return "Bishop";
-        case Knight :
-            return "Knight";
-        case Rook :
-            return "Rook";
-        case King :
-            return "King";
-        case Queen :
-            return "Queen";
-        case Blank :
-            return "Blank";
-    }
-    return "";
-}
-// return the points of the vector
-int get_points(std::vector< piece > vec){
-    std::vector<int> vals;
-    vals.push_back(1);
-    vals.push_back(3);
-    vals.push_back(3);
-    vals.push_back(5);
-    vals.push_back(9);
-    vals.push_back(0);
-
-    int sum = 0;
-    for(size_t i = 0; i < vec.size(); ++i){
-        sum += vals[vec[i].type];
-    }
-    return sum;
-}
-
-char get_board_symbol(piece piece){
-    // not sure how to do this but I weant these unicodes
-    // https://en.wikipedia.org/wiki/Chess_symbols_in_Unicode
-    int color = piece.color;
-    if(color == White){
-        switch(piece.type) {
-            case Pawn :
-                return 'P';
-            case Bishop :
-                return 'B';
-            case Knight :
-                return 'K';
-            case Rook :
-                return 'R';
-            case King :
-                return 'X';
-            case Queen :
-                return 'Q';
-            case Blank :
-                return 'o';
-        }
-    }
-    switch(piece.type) {
-        case Pawn :
-            return 'p';
-        case Bishop :
-            return 'b';
-        case Knight :
-            return 'k';
-        case Rook :
-            return 'r';
-        case King :
-            return 'x';
-        case Queen :
-            return 'q';
-        case Blank :
-            return 'o';
-    }
-    return '\0';
-}
-
-bool valid_move(std::string play, bool check){
-    if(play.length() != 5){
-        return false;
-    }
-    if(letter_to_ind(play[0]) > 7 || letter_to_ind(play[0]) < 0){
-        return false;
-    }
-    if(letter_to_ind(play[3]) > 7 || letter_to_ind(play[3]) < 0){
-        return false;
-    }
-    if(num_to_ind(play[1]) > 7 || num_to_ind(play[1]) < 0){
-        return false;
-    }
-    if(num_to_ind(play[4]) > 7 || num_to_ind(play[4]) < 0){
-        return false;
-    }
-    if(play[2] != ' '){
-        return false;
-    }
-    std::pair<std::pair<int, int>, std::pair<int, int>> to_from = get_move_indices(play);
-    piece *moved_piece = &board[to_from.first.first][to_from.first.second];
-    piece *new_location = &board[to_from.second.first][to_from.second.second];
-    if(moved_piece->color != turn){
-        return false;
-    }
-    if(new_location->type != Blank && new_location->color == turn){
-        return false;
-    }
-    if(!legal_move(moved_piece, to_from, check)){
-        return false;
-    }
-    return true;
-}
-
-void make_move(std::string play){
-    std::pair<std::pair<int, int>, std::pair<int, int>> to_from = get_move_indices(play);
-    piece *moved_piece = &board[to_from.first.first][to_from.first.second];
-    piece *new_location = &board[to_from.second.first][to_from.second.second];
-
-    piece taken_piece = board[to_from.second.first][to_from.second.second];
-    new_location->type = moved_piece->type;
-    new_location->color = moved_piece->color;
-
-    moved_piece->type = Blank;
-    moved_piece->color = -1;
-    
-    add_to_taken(taken_piece);
-
-    return;
-}
-// 0 is neither check nor mate, 1 is check, 2 is mate -- this is for the other side (ie if it is whites turn, we are checking if the black king is checked)
-int check_checkmate(int turn){
-    std::pair<int, int> king = get_king_address(turn);
-    for(int row = 0; row < 8; ++row){
-        for(int col = 0; col < 8; ++col){
-            
-        }
-    }
-    return 0;
-}
-
 void print_instructions(){
-    cout << "Let's play chess: White moves first!" << endl;
+    cout << "Let's play poker: This will track stacks, allow for rebuys, or new players, and keep track of raises etc" << endl;
     cout << "Usage: in order to move a piece from a5 to a6, type command a5 a6." << endl;
     cout << "ie src dst;" << endl << endl;
 }
-// return row col in the vector
-std::pair<std::pair<int, int>, std::pair<int, int>> get_move_indices(std::string play){
-    int src_col = letter_to_ind(play[0]);
-    int src_row = num_to_ind(play[1]);
-    int dst_col = letter_to_ind(play[3]);
-    int dst_row = num_to_ind(play[4]);
 
-    std::pair<int, int> src = std::make_pair(src_row, src_col);
-    std::pair<int, int> dst = std::make_pair(dst_row, dst_col);
+void error_in_player_entry(){
+    cout << "Please enter one of: " << endl;
+    cout << "\"N name chipCount\""  << endl;
+    cout << "\"C name chipCount\"" << endl;
+    cout << "\"B bigBlind littleBlind\"" << endl;
+}
+void get_players(){
+    cout << "Please enter the name and chip count of each player you would like to add like this: \"N name chipCount\""  << endl;
+    cout << "if you make a mistake, please enter \"C name chipCount\"" << endl;
+    cout << "once you are finished, enter \"B bigBlind littleBlind\"" << endl;
+
+    std::string name;
+    char instruction;
+    std::string buy_in;
     
-    return std::make_pair(src, dst);
-}
-
-void add_to_taken(piece taken_piece){
-    if(taken_piece.type == Blank){
-        return;
-    }
-    if(takenPieces[turn].find(taken_piece.type) == takenPieces[turn].end()){
-        takenPieces[turn][taken_piece.type] = 0;
-    }
-    takenPieces[turn][taken_piece.type] += 1;
-    return;
-}
-            
-int letter_to_ind(char val){
-    return val - 'a';
-}
-int num_to_ind(char val){
-    return val - '1';
-}
-
-bool legal_move(piece *moved_piece, std::pair<std::pair<int, int>, std::pair<int, int>> to_from, bool check){
-    return true;
-}
-
-// find the address of the king !turn (white turn, black king) (-1 and -1 if not there)
-std::pair<int, int> get_king_address(int turn){
-    for(int row = 0; row < 8; ++row){
-        for(int col = 0; col < 8; ++col){
-            if(board[row][col].type == King && board[row][col].color == !turn){
-                return std::make_pair(row, col);
-            }
+    bool exit = false;
+    while(!exit){
+        cin >> instruction >> name >> buy_in;
+        switch(instruction) {
+            case 'N'  :
+                if( add_player(name, std::stoi(buy_in)) ){
+                    break;
+                }
+                error_in_player_entry();
+            case 'C'  :
+                if( fix_player(name, std::stoi(buy_in)) ){
+                    break;
+                }
+                error_in_player_entry();
+            case 'B'  :
+                exit = true;
+                big_blind = std::stoi(name);
+                little_blind = std::stoi(buy_in);
+                break;
+            default :
+                error_in_player_entry();
         }
     }
-    return std::make_pair(-1, -1);
+    
+    
+}
+
+bool add_player(std::string player_name, int player_chips){
+    if(players.find(player_name) != players.end()){
+        cout << "player name already taken!" << endl;
+        return false;
+    }
+    if(player_chips < 0){
+        cout << "please enter a positive stack" << endl;
+        return false;
+    }
+    else{
+        cout << "Player: " << player_name << ", Stack: " << player_chips << endl;
+        players[player_name] = new Player(player_name, player_chips);
+        return true;
+    }
+}
+bool fix_player(std::string player_name, int player_chips){
+    
 }
--- poker_chip_counter/poker_chip_counter/globals.h
+++ poker_chip_counter/poker_chip_counter/globals.h
@@ -15,57 +15,32 @@ using std::cout;
 using std::cin;
 using std::endl;
 using std::min;
-enum pieceType {Pawn = 0, Bishop = 1, Knight = 2, Rook = 3, Queen = 4, King = 5, Blank = 6};
 
-class piece
+class Player
 {
     public:
-    piece(int Color, pieceType Type){
-        type = Type;
-        color = Color;
+    Player(std::string name_str, int buy_in){
+        stack = buy_in;
+        name = name_str;
     };
-    pieceType type;
-    int color;
+    void re_buy(int rebuy){
+        stack += rebuy;
+    }
+    std::string name;
+    int stack;
 };
 
-// for indexing into data structures
-const int White = 0;
-const int Black = 1;
-// length 2, taken[White] is the map of pieces to quantity that white has taken from Black
-extern std::vector< std::unordered_map< pieceType, int > > takenPieces;
-// this is stored from whites perspective -- this means that a1 is indexed at [1][1]
-extern std::vector< std::vector< piece > > board;
-// whose turn is it
-extern int turn;
-// how many moves
-extern int move;
-// return string of the piece
-std::string get_type(piece type);
-// return the points of the vector
-int get_points(std::vector< piece > vec);
-// cout the space followed by an empty space
-void print_space(piece type);
-// return the symbol to be printed on the board
-char get_board_symbol(piece type);
-// check that, given who's turn it is and the move, is it valid
-bool valid_move(std::string play, bool check);
-// make the move
-void make_move(std::string play);
-// check if there is a checkmate
-int check_checkmate(int turn);
-// print the instructions
-void print_instructions();
-// get the indices in the vector of the moves
-std::pair<std::pair<int, int>, std::pair<int, int>> get_move_indices(std::string play);
-// add the taken piece to the data stucture
-void add_to_taken(piece taken_piece);
-// convert letter to an indices
-int letter_to_ind(char val);
-// convert an int char to ind
-int num_to_ind(char val);
-// can this piece move like this
-bool legal_move(piece *moved_piece, std::pair<std::pair<int, int>, std::pair<int, int>> to_from, bool check);
-// find the address of the king !turn (white turn, black king)
-std::pair<int, int> get_king_address(int turn);
+// this holds all the players
+extern std::unordered_map< std::string, Player* > players;
+// this is the table of players
+extern std::vector< Player* > table;
 
+extern int little_blind;
+extern int big_blind;
+
+void print_instructions();
+void get_players();
+void error_in_player_entry();
+bool add_player(string player_name, int player_chips);
+bool fix_player(string player_name, int player_chips);
 #endif
